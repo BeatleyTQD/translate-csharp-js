1631666b4fab3c24482405c2f9589ded182fe9f5
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-08-21 11:45:55 -0500

    Translated Place Names from C# to JS

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,22 @@
 function main() {
   // Put your code here
+  let placeNames = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+  console.log("All Place Names")
+
+  for (let place of placeNames) {
+    console.log(place)
+  }
+
+  console.log("")
+  const startsWithThe = placeNames.filter((place) => place.startsWith("The"));
+
+  console.log("'The' Place Names")
+
+  for (let place of startsWithThe) {
+    console.log(place)
+  }
 
 }
 
